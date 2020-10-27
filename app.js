0e170284520fd6b37888d14b9065bc408da53acf
Jessy DEFRASNES <Jessydefrasnes242@hotmail.com>
2020-10-27 03:01:29 +0100

    Polish

--- app.js
+++ app.js
@@ -16,9 +16,7 @@ app.get("/", (req, res) => {
 });
 
 //localhost
-http.listen(3000, () => {
-  console.log("listening on *:3000");
-});
+http.listen(3000);
 
 //server send welcome message
 let msg = "Welcome to FindMe";
